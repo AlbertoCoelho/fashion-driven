e83ccaf6397fcb22d15737fa7d9f14a65ee87706
Alberto <tejotaesi@gmail.com>
2021-11-28 15:28:37 -0300

    Mostrar blusas encomendadas recentemente

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let contadorItems = 0;
+const dizerNome = prompt("Qual o seu nome?");
+let pedidos = [];
 
 function  selecionarModelo(elemento){
+    console.log(elemento);
     const selecionado = document.querySelector(".modelo .selecionado");
 
     if (selecionado !== null) {
@@ -58,21 +61,39 @@ function verificarPedido() {
     }
 }
 
-function confirmarPedido(botao){
+function carregarPedidos(){
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/shirts-api/shirts");
 
-    function pedidoConfirmado(resposta){
-        alert("Seu pedido foi confirmado!");   
-    }
+    
+    function carregandoPedido(resposta){
+        console.log(resposta.data);
+        pedidos = resposta.data;
 
+        const ulPedidos = document.querySelector(".imagem-autor-pedido");
+        ulPedidos.innerHTML = "";
+
+        
+        for(let i=0; i<pedidos.length;i++){
+            const pedido = pedidos[i];
+
+            ulPedidos.innerHTML += 
+            `
+            <li class="pedido">
+                <img src="${pedido.image}" />
+                <span class="criador" >Criador: ${pedido.owner}</span>
+            </li>
+            `
+        }              
+    }
+    
     function pedidoNegado(resposta){
         alert("Ops, não conseguimos processar sua encomenda");
     }
-
-    promise.then(pedidoConfirmado);
+    
+    promise.then(carregandoPedido);
     promise.catch(pedidoNegado);
 }
-confirmarPedido();
+carregarPedidos();
 
 
 function inputComString(){
@@ -93,6 +114,57 @@ function inputComString(){
     verificarPedido();
 }
 inputComString();
+
+
+function pedidoSucesso(resposta){
+    window.alert("Encomenda feita!");
+    const ulPedidos = document.querySelector(".imagem-autor-pedido");
+    const pedido = resposta.data;
+    ulPedidos.innerHTML += 
+    `
+    <li class="pedido">
+        <img src="${pedido.image}" />
+        <span class="criador" >Criador: ${pedido.owner}</span>
+    </li>
+    `
+    
+    const input = document.querySelector(".input");
+    input.value = '';  
+
+    recarregarPagina();
+
+}
+
+function pedidoFalhou(){
+    console.log("Deu erro na sua encomenda!");
+}
+
+
+function enviarPedido(){
+    let input = document.getElementById("meuinput");
+    let link = input.value;
+    console.log(link);
+
+    const pedido = {
+        model: "t-shirt",
+	    neck: "v-neck",
+	    material: "silk",
+        owner: dizerNome,  
+        image: link,
+        author: dizerNome
+    };
+    console.log(pedido);
+
+    const enviandoPedido = axios.post("https://mock-api.driven.com.br/api/v4/shirts-api/shirts", pedido);
+
+    enviandoPedido.then(pedidoSucesso);
+    enviandoPedido.catch(pedidoFalhou);
+}
+
+function recarregarPagina(){
+    carregarPedidos();
+
+}
 
 /*
 function verificarURL(){
